9cc0e35125c9be9aa59ae65e744d4b8c5625e6c1
Tony Vu <anthonyqvu@gmail.com>
2017-12-14 15:58:49 -0600

    Fix outbound call callback

--- outbound-call-callback.js
+++ outbound-call-callback.js
@@ -1,52 +1,36 @@
 exports.handler = function(context, event, callback) {
-  let jwt = require('jsonwebtoken')
-  let response = new Twilio.Response()
-
-  response.appendHeader('Access-Control-Allow-Origin', '*');
-  response.appendHeader('Access-Control-Allow-Methods', 'POST');
-  response.appendHeader('Content-Type', 'application/json');
-  response.appendHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-  const to = event.to
-  const from = event.from
-  const conferenceSid = event.sid
-  const token = event.token
+  console.log(event)
+  const toPhone = event.ToPhone
+  const fromPhone = event.FromPhone
+  const conferenceSid = event.Sid
   const client = context.getTwilioClient();
 
-  jwt.verify(token, context.AUTH_TOKEN, function(err, decoded) {
-    if(err) {
-      response.appendHeader('Status', 401)
-      callback(null, response)
-    } else {
-      client
-        .conferences(conferenceSid)
-        .participants.create({to: to, from: from, earlyMedia: "true"})
-        .then((participant) => {
-          const voice_response = new Twilio.VoiceResponse();
+  client
+    .conferences(conferenceSid)
+    .participants.create({to: toPhone, from: fromPhone, earlyMedia: "true"})
+    .then((participant) => {
 
-          const dial = voice_response.dial();
-          dial.conference({
-            beep: false,
-            waitUrl: '',
-            startConferenceOnEnter: true,
-            endConferenceOnExit: false
-          }, conferenceSid);
+      const resp = new Twilio.twiml.VoiceResponse();
+      const dial = resp.dial();
 
-          // Now update the task with a conference attribute with Agent Call Sid
-          client.taskrouter.v1
-            .workspaces(context.TWILIO_WORKSPACE_SID)
-            .tasks(conferenceSid)
-            .update({
-              attributes: JSON.stringify({conference: {sid: participant.conferenceSid, participants: {worker: participant.callSid, customer: ""}}}),
-            }).then((task) => {
-              console.log(task)
-            })
+      dial.conference({
+        beep: false,
+        waitUrl: '',
+        startConferenceOnEnter: true,
+        endConferenceOnExit: false
+      }, conferenceSid);
 
-          callback(null, voice_response)
-        })
-        .catch((error) => {
-          console.log(error)
-        })
-    }
-  })
+      // Now update the task with a conference attribute with Agent Call Sid
+      // MOVE THIS TO ANOTHER FUNCTION
+      // client.taskrouter.v1
+      //   .workspaces(context.TWILIO_WORKSPACE_SID)
+      //   .tasks(conferenceSid)
+      //   .update({
+      //     attributes: JSON.stringify({conference: {sid: participant.conferenceSid, participants: {worker: participant.callSid, customer: ""}}}),
+      //   }).then((task) => {
+      //     console.log(task)
+      //   })
+      //
+      callback(null, resp)
+    })
 };
